75c72d616225aadcb86ac028616bfa45f5fe8790
Tyrannas <someonefefe@gmail.com>
2015-04-04 12:35:54 +0200

    modifs users

--- public/javascript/listMaker.js
+++ public/javascript/listMaker.js
@@ -2,7 +2,7 @@ module.exports = {
 	make: function(array){
 		var res ="<div id='container-users'>";
 		for(i in array){
-			res+="<em class='user'>"+array[i]+'</em>';
+			res+="<span class='user'>"+array[i]+'</span>';
 		}
 		res += '</div>';
 		return res;
