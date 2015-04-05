2f10ff2603d7c10fc81d2568ead994ed4d0276aa
David Gödicke <godicked@gmail.com>
2015-04-05 19:23:10 +0200

    change profil to chat

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,7 @@ module.exports = function(app, passport) {
     // app.post('/login', do all our passport stuff here);
 	// process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/chat', // redirect to the secure chat section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -40,7 +40,7 @@ module.exports = function(app, passport) {
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
 	 app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/chat', // redirect to the secure chat section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -49,9 +49,9 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
+    app.get('/chat', isLoggedIn, function(req, res) {
 		if(req.user.local.name){
-			res.render('profile.ejs', {
+			res.render('chat.ejs', {
 				user : req.user // get the user out of session and pass to template
 			});
 		}
@@ -72,7 +72,7 @@ module.exports = function(app, passport) {
 		console.log(pseudo);
 		User.findByIdAndUpdate(req.user._id, { $set: { 'local.name': pseudo }}, function (err, user) {
 			req.user = user;
-			res.redirect('/profile');
+			res.redirect('/chat');
 		});
 	});
 
