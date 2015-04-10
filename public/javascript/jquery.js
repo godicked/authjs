5109c6d72c3ea8e85aa89811998a60c8c4968f7f
Félix Alié <someonefefe@gmail.com>
2015-04-10 12:13:48 +0200

    fix

--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -111,6 +111,7 @@
                     else
                     {
                         $('#div_fond_popup').show(0);
+                        $('#div_form_pass').show(0);
                         $('#entrer_room_pass').focus();
                         entrer_room(content);
                     }
@@ -128,6 +129,7 @@
                         };
                     socket.emit('room',data);
                     $('#div_form_pass').hide(0);
+                    $('#div_fond_popup').hide(0);
                     return false;
                 });
             }
